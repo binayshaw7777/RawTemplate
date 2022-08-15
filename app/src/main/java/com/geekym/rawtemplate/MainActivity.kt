09e399e4e0ddffda496bd4de0663c7eccabf74ca
Binay Shaw <binayshaw7777@gmail.com>
2022-08-15 20:49:16 +0530

    (-) removed Deprecated setOnNavigationItemSelectedListener code

--- app/src/main/java/com/geekym/rawtemplate/MainActivity.kt
+++ app/src/main/java/com/geekym/rawtemplate/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.geekym.rawtemplate
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -12,11 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var bottomNavigationView: BottomNavigationView? = null
         myFragment(HomeFragment())
 
-        bottomNavigationView = findViewById(R.id.bottomNav)
-        bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item: MenuItem ->
+        val bottomNavigationView: BottomNavigationView? = findViewById(R.id.bottomNav)
+
+        bottomNavigationView?.setOnItemSelectedListener { item ->
             var fragment: Fragment? = null
             when (item.itemId) {
                 R.id.home -> fragment = HomeFragment()
@@ -25,9 +24,9 @@ class MainActivity : AppCompatActivity() {
                 R.id.profile -> fragment = ProfileFragment()
             }
             myFragment(fragment)
-        })
-
+        }
     }
+
     private fun myFragment(homeFragment: Fragment?): Boolean {
         if (homeFragment != null) {
             supportFragmentManager.beginTransaction().replace(R.id.frame, homeFragment).commit()
