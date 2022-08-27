73537ee4e26d74e4996dfc2c4d3794bb61bdacbb
Binay Shaw <binayshaw7777@gmail.com>
2022-08-28 00:51:35 +0530

    (+) User data class added

--- app/src/main/java/com/geekym/rawtemplate/Introduction/Splashscreen.kt
+++ app/src/main/java/com/geekym/rawtemplate/Introduction/Splashscreen.kt
@@ -4,17 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.geekym.rawtemplate.MainActivity
 import com.geekym.rawtemplate.R
+import com.google.firebase.auth.FirebaseAuth
 
 class Splashscreen : AppCompatActivity() {
 
     lateinit var handler: Handler
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
         supportActionBar?.hide()
+        firebaseAuth = FirebaseAuth.getInstance()
 
         handler = Handler()
         handler.postDelayed({
@@ -23,7 +27,21 @@ class Splashscreen : AppCompatActivity() {
             val intent = Intent(this, Onboarding::class.java)
             startActivity(intent)
             finish()
-        } , 1500) // here we're delaying to startActivity after 2 seconds
+        } , 2500) // here we're delaying to startActivity after 2 seconds
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (firebaseAuth.currentUser != null) {
+            handler = Handler()
+            handler.postDelayed({
+
+                // Delay and Start Activity
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } , 2500) // here we're delaying to startActivity after 2 seconds
+        }
     }
 }
