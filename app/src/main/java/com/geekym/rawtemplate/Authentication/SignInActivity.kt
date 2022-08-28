cc3a366e3968d34ddf9272054c30832c33af0182
Aritra Das <aritrarick2002@gmail.com>
2022-08-28 15:49:41 +0530

    Password show/hide done in Sign In Screen

--- app/src/main/java/com/geekym/rawtemplate/Authentication/SignInActivity.kt
+++ app/src/main/java/com/geekym/rawtemplate/Authentication/SignInActivity.kt
@@ -1,11 +1,16 @@
 package com.geekym.rawtemplate.Authentication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.util.Patterns
+import android.view.MotionEvent
 import android.widget.Toast
 import com.geekym.rawtemplate.MainActivity
+import com.geekym.rawtemplate.R
 import com.geekym.rawtemplate.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -14,6 +19,7 @@ class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
@@ -22,6 +28,47 @@ class SignInActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         firebaseAuth = FirebaseAuth.getInstance()
+
+        var passwordVisible = false
+
+        binding.loginpassword.setOnTouchListener { v, event ->
+            val Right = 2
+            if (event.getAction() === MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= binding.loginpassword.getRight() - binding.loginpassword.getCompoundDrawables()
+                        .get(Right).getBounds().width()
+                ) {
+                    val selection: Int = binding.loginpassword.getSelectionEnd()
+                    //Handles Multiple option popups
+                    if (passwordVisible) {
+                        //set drawable image here
+                        binding.loginpassword.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                            0,
+                            0,
+                            R.drawable.visibility_off,
+                            0
+                        )
+                        //for hide password
+                        binding.loginpassword.setTransformationMethod(PasswordTransformationMethod.getInstance())
+                        passwordVisible = false
+                    } else {
+                        //set drawable image here
+                        binding.loginpassword.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                            0,
+                            0,
+                            R.drawable.visibility,
+                            0
+                        )
+                        //for show password
+                        binding.loginpassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance())
+                        passwordVisible = true
+                    }
+                    binding.loginpassword.setLongClickable(false) //Handles Multiple option popups
+                    binding.loginpassword.setSelection(selection)
+                    return@setOnTouchListener true
+                }
+            }
+            false
+        }
 
         binding.frgtpass.setOnClickListener {
             startActivity(Intent(this, ForgotPassword::class.java))
