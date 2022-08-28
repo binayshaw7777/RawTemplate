b22a776616f0422edb6a1ba5f5b742883ae078b7
Binay Shaw <binayshaw7777@gmail.com>
2022-08-28 15:17:55 +0530

    (+) Password show/hide (Aritra)

--- app/src/main/java/com/geekym/rawtemplate/Authentication/SignUpActivity.kt
+++ app/src/main/java/com/geekym/rawtemplate/Authentication/SignUpActivity.kt
@@ -1,10 +1,15 @@
 package com.geekym.rawtemplate.Authentication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.util.Patterns
+import android.view.MotionEvent
 import android.widget.Toast
+import com.geekym.rawtemplate.R
 import com.geekym.rawtemplate.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -17,6 +22,7 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var db: DatabaseReference
     private lateinit var fd: FirebaseDatabase
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
@@ -27,6 +33,86 @@ class SignUpActivity : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
         fd = FirebaseDatabase.getInstance()
         db = fd.getReference("Users")
+
+        var passwordVisible = false
+
+        binding.signuppass.setOnTouchListener { v, event ->
+            val Right = 2
+            if (event.getAction() === MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= binding.signuppass.getRight() - binding.signuppass.getCompoundDrawables()
+                        .get(Right).getBounds().width()
+                ) {
+                    val selection: Int = binding.signuppass.getSelectionEnd()
+                    //Handles Multiple option popups
+                    if (passwordVisible) {
+                        //set drawable image here
+                        binding.signuppass.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                            0,
+                            0,
+                            R.drawable.visibility_off,
+                            0
+                        )
+                        //for hide password
+                        binding.signuppass.setTransformationMethod(PasswordTransformationMethod.getInstance())
+                        passwordVisible = false
+                    } else {
+                        //set drawable image here
+                        binding.signuppass.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                            0,
+                            0,
+                            R.drawable.visibility,
+                            0
+                        )
+                        //for show password
+                        binding.signuppass.setTransformationMethod(HideReturnsTransformationMethod.getInstance())
+                        passwordVisible = true
+                    }
+                    binding.signuppass.setLongClickable(false) //Handles Multiple option popups
+                    binding.signuppass.setSelection(selection)
+                    return@setOnTouchListener true
+                }
+            }
+            false
+        }
+
+        binding.confirmpass.setOnTouchListener { view, event ->
+            val Right = 2
+            if (event.getAction() === MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= binding.confirmpass.getRight() - binding.confirmpass.getCompoundDrawables()
+                        .get(Right).getBounds().width()
+                ) {
+                    val selection: Int = binding.confirmpass.getSelectionEnd()
+                    //Handles Multiple option popups
+                    if (passwordVisible) {
+                        //set drawable image here
+                        binding.confirmpass.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                            0,
+                            0,
+                            R.drawable.visibility_off,
+                            0
+                        )
+                        //for hide password
+                        binding.confirmpass.setTransformationMethod(PasswordTransformationMethod.getInstance())
+                        passwordVisible = false
+                    } else {
+                        //set drawable image here
+                        binding.confirmpass.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                            0,
+                            0,
+                            R.drawable.visibility,
+                            0
+                        )
+                        //for show password
+                        binding.confirmpass.setTransformationMethod(HideReturnsTransformationMethod.getInstance())
+                        passwordVisible = true
+                    }
+                    binding.confirmpass.setLongClickable(false) //Handles Multiple option popups
+                    binding.confirmpass.setSelection(selection)
+                    return@setOnTouchListener true
+                }
+            }
+            false
+        }
 
         // For Sign In text
         binding.gotosignin.setOnClickListener {
@@ -43,7 +129,10 @@ class SignUpActivity : AppCompatActivity() {
             //Create user object
             val user = User(name, email)
 
-            if (name.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && confirmpass.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            if (name.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && confirmpass.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(
+                    email
+                ).matches()
+            ) {
                 if (pass == confirmpass && pass.length > 8) {
 
                     firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
@@ -55,7 +144,11 @@ class SignUpActivity : AppCompatActivity() {
                             db.child(u?.uid!!).setValue(user).addOnCompleteListener { it1 ->
                                 if (it1.isSuccessful) {
                                     u.sendEmailVerification()
-                                    Toast.makeText(this, "Email Verification sent to your mail", Toast.LENGTH_LONG).show()
+                                    Toast.makeText(
+                                        this,
+                                        "Email Verification sent to your mail",
+                                        Toast.LENGTH_LONG
+                                    ).show()
                                     startActivity(Intent(this, SignInActivity::class.java))
                                 }
                             }
